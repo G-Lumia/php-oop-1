3b6b0c87b2a72398dff82639b57765ccfbc4fd75
G-Lumia <giorgiolumia@gmail.com>
2023-05-16 15:10:19 +0200

    book class created

---
+++ models/books.php
@@ -0,0 +1,19 @@
+<?php 
+    class Book {
+        public $title;
+        public $cost;
+        public $genre;
+        public $genre2;
+
+        public function __construct(string $title, int $cost, string $genre) 
+        {
+            $this->title = $title;
+            $this->cost = $cost;
+            $this->genre = $genre;
+        }
+
+        public function printBook():string{
+            return "<span> $this->title, $this->cost, $this->genre </span>";
+        }
+    }
+?>
