9cd3d25b1700f979c8406f1cb5c0a00aa85ad96f
G-Lumia <giorgiolumia@gmail.com>
2023-05-16 15:36:56 +0200

    bonus 1 done

--- index.php
+++ index.php
@@ -2,9 +2,12 @@
 
 include __DIR__ . '/models/books.php';
 
-$libro1 = new Book('Topolino', 450, 'comico');
-$libro2 = new Book('Diabolik' , 200, 'criminale');
+$libro1 = new Book('Topolino', 450, 'comico', 'infanzia');
+$libro2 = new Book('Diabolik' , 200, 'criminale' , 'noir');
+$libro3 = new Book('Peppa Pig' , 123, 'infanzia');
+
 
 echo $libro1->printBook();
 echo $libro2->printBook();
+echo $libro3->printBook();
 ?>
--- models/books.php
+++ models/books.php
@@ -5,15 +5,16 @@
         public $genre;
         public $genre2;
 
-        public function __construct(string $title, int $cost, string $genre) 
+        public function __construct(string $title, int $cost, string $genre, string $genre2 = null) 
         {
             $this->title = $title;
             $this->cost = $cost;
             $this->genre = $genre;
+            $this->genre2 = $genre2;
         }
 
         public function printBook():string{
-            return "<span> $this->title, $this->cost, $this->genre <br> </span>";
+            return "<span> Titolo: $this->title Costo: $this->cost euro Genere: $this->genre $this->genre2 <br> </span>";
         }
     }
 ?>
